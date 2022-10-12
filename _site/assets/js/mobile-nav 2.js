e083c6bd19dd65f45c0728db50d9c6008ad665f0
Shoham Letzter <sletzter@Shohams-Air.lan>
2022-10-12 11:07:52 +0100

    changed date

--- _site/assets/js/mobile-nav 2.js
+++
@@ -1,9 +0,0 @@
-//Modified tutorial from W3Schools
-//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav
-function openNav() {
-  document.getElementById("mySidenav").classList.add('show');
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").classList.remove('show');
-}
--- _site/readme 2.md
+++
@@ -1,3 +0,0 @@
-# Hi
-
-* UCL Combinatorics Seminar webpage
